6f569f95e28ece0ed5e0b8a6505fcd419793a19b
Omar <omargfm33.com>
2024-07-08 19:56:32 -0400

    modificaciones

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,25 +1,40 @@
-import { MenuList } from "../helpers/MenuList";
 import MenuItem from "../components/MenuItem.jsx";
 import "../styles/Menu.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const baseURL = 'https://restaurant-backend-ruby.vercel.app/api/';
 
 function Menu() {
-    return (
-        <div className="menu">
-            <h1 className="menuTitle">Nuestro menú</h1>
-            <div className="menuList">
-                {MenuList.map((menuItem, key) => {
-                    return (
-                        <MenuItem
-                            key={key}
-                            image={menuItem.image}
-                            name={menuItem.name}
-                            price={menuItem.price}
-                        />
-                    );
-                })}
+
+    const [products, setProducts] = useState(null);
+
+    useEffect(() => {
+        axios.get(baseURL + 'products').then((response) => {
+            setProducts(response.data);
+        });
+    }, []);
+
+    if (products) {
+
+        return (
+            <div className="menu">
+                <h1 className="menuTitle">Nuestro menú</h1>
+                <div className="menuList">
+                    {products.list.map((product, key) => {
+                        return (
+                            <MenuItem
+                                key={key}
+                                image={baseURL + "img/" + product.img}
+                                name={product.name}
+                                price={product.price}
+                            />
+                        );
+                    })}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Menu;
